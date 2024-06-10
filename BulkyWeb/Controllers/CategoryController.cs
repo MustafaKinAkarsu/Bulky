ad8f1890d1610ff176403b8241b0789b2c74deda
Mustafa Akin Akarsu <akarsumustafa0153@gmail.com>
2024-06-10 18:29:46 +0300

    Update added

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -24,10 +24,10 @@ namespace BulkyWeb.Controllers
         [HttpPost]
 		public IActionResult Create(Category obj)
 		{
-            //if(obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("name", "The Display Order cannot match the name");
-            //}
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot match the name");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj); // Keeping track of what to add
@@ -35,6 +35,31 @@ namespace BulkyWeb.Controllers
 				return RedirectToAction("Index"); // To return to the Index Action. (To see updated Categories)
 			}
             return View();
+		}
+		public IActionResult Edit(int? id)
+		{
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+			//Category? categoryFromDb = _db.Categories.FirstOrDefault(u=> u.Name =="fdfsdfs") or Category? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Name.Contains("fasdfa");
+			//Advantage of this is it does not require primary key unlike Find
+			//Category? categoryFromDb = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+			if (categoryFromDb == null) NotFound();
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+		
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj); // Keeping track of what to Update
+				_db.SaveChanges(); // Saving the changes and adding to the database
+				return RedirectToAction("Index"); // To return to the Index Action. (To see updated Categories)
+			}
+			return View();
 		}
 	}
 }
